a872286306424465f30c988b1326905b3dbff411
Jinwon Lee <dwdljw@gmail.com>
2024-01-08 00:14:02 -0800

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import TodoApp from "./TodoApp";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App(){
+    return (
+        <TodoApp/> // TodoApp contains all the logic
+    )
+}
+
+export default App;
